f0516fef745f7874343f0c764342ff7eb3ea7d58
Karthik Sridharan <karthik@flexiple.com>
2018-11-08 15:14:24 +0530

    Initial commit with host of changes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    #@article.body = params[:article][:body]
+    flash.notice = "Article created"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    flash.notice = "Article '#{@article.title}' updated"
+    redirect_to article_path
+  end
+
+end
